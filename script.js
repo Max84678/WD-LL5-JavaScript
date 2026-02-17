9be79c08be82010aefadb687110ed14d77863d5f
Max84678 <164530658+Max84678@users.noreply.github.com>
2026-02-17 19:19:33 +0000

    Worked on JS to learn console.log, console.error, and alert

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+
+console.error("This is my first JavaScript error!");
+
+alert("Liam Kelley");
+alert("Edgar Vaquez Gonzalez");
+alert("Gary Peterson");
+alert("Venkata Teja Gogulampati");
+alert("Tri Nguyen");
+
+console.log(console);
